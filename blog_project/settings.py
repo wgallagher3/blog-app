58693467415cce4d527e228c75816cbdd3cf6f79
William Gallagher <wgallagher3@gmail.com>
2019-09-19 17:48:44 -0500

    whitenoise fix

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFileStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
