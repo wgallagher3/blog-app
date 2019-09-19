3b08d810fa5a2ecb2d3ed37fe695aff1a87cffca
William Gallagher <wgallagher3@gmail.com>
2019-09-19 17:07:41 -0500

    user accounts

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
     # My apps
     'blog.apps.BlogConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
